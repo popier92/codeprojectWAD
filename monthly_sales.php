52d2556cc3ef2b2b96be9ea3e2293d290934cb52
Popier92 <popier92@gmail.com>
2025-01-03 09:41:59 +0800

    Monthly sales 1st pie chart

--- monthly_sales.php
+++ monthly_sales.php
@@ -40,8 +40,22 @@
         <!-- Main Content Section -->
      <div class="main-content">
         <h1>Monthly Sales Report</h1>
-        <div class="graph-placeholder">
-            Graph Placeholder (Insert your chart or visualization here)
+        <div class="pie-chart">
+            <!--Graph Placeholder (Insert your chart or visualization here)-->
+            <div class="pie-chart-orders" style="max-width:180px;flex:0 0 180px;" >
+                <svg viewBox="0 0 110 115" >
+                    <path class="grey" d="M55,25 A40,40 0 1,1 54,25 A40,40 0 0,1 55,25" fill="none" stroke="rgba(0,0,0,.1)" stroke-width="20" stroke-linecap="butt"></path>
+                    <path id="color" stroke="rgb(224, 60, 60)" stroke-dasharray="251" stroke-width="20" stroke-dashoffset="0" fill="none" class="color" d="M55,25 A40,40 0 1,1 54,25 A40,40 0 0,1 55,25" stroke-linecap="butt"></path>
+                    <text x="55" y="66" text-anchor="middle" font-size="11" class="fwheadings">Total Orders</text>
+                </svg>
+            </div>
+            <div class="pie-chart-orders" style="max-width:180px;flex:0 0 180px;" >
+                <svg viewBox="0 0 110 115" >
+                    <path class="grey" d="M55,25 A40,40 0 1,1 54,25 A40,40 0 0,1 55,25" fill="none" stroke="rgba(0,0,0,.1)" stroke-width="20" stroke-linecap="butt"></path>
+                    <path id="color" stroke="rgb(224, 60, 60)" stroke-dasharray="250" stroke-width="20" stroke-dashoffset="100" fill="none" class="color" d="M55,25 A40,40 0 1,1 54,25" stroke-linecap="butt"></path>
+                    <text x="55" y="66" text-anchor="middle" font-size="11" class="fwheadings">Total Orders</text>
+                </svg>
+            </div>
         </div>
      </div>
     </div>
