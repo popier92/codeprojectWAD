e0b93981ad5a097badab4a45f22e6738eaa87152
Popier92 <popier92@gmail.com>
2025-01-04 15:13:05 +0800

    monthly sales fix

--- monthly_sales.php
+++ monthly_sales.php
@@ -45,109 +45,34 @@
         <div class="main-content">
             <h1>Monthly Sales Report</h1>
             <div style="display : flex">
-                <div class="pie-chart">
+                <div class="chart">
                     <!--Graph Placeholder (Insert your chart or visualization here)-->
-                    <canvas id="order-chart" width="100" height="100" style="width: 200x;;max-width:200px"></canvas>
-                    <canvas id="growth-chart" width="100" height="100" style="width: 200x;;max-width:200px"></canvas>
-                    <canvas id="revenue-chart" width="100" height="100" style="width: 200x;;max-width:200px"></canvas>
+                    <canvas id="chart" width="900" height="400" style="width: 900x;;max-width:900px"></canvas>
+
                     <script>
-                        var yValues = [26, 49, 60];
-                        var barColors = [
-                            "#b91d47",
-                            "#f1bcbc"
-                        ];
-                        new Chart("order-chart",
-                            {
-                                type: "doughnut",
-                                data:
-                                {
-                                    datasets: [{
-                                        backgroundColor: ["#b91d47", "#f1bcbc"],
-                                        data: [yValues[0], 100 - yValues[0]]
-                                    }]
-                                },
-                                options:
-                                {
-                                    title: {
-                                        display: true,
-                                        text: "Total Orders"
-                                    }
-                                }
-                            });
+                        var xValues = ["january", "febuary", "march", "april", "may","june","july","august","september","october","november","december"];
+                        var yValues = [55, 49, 44, 24, 15,22,33,55,77,55,33,22];
+                        var barColors = ["red", "green", "blue", "orange", "brown"];
 
-                        new Chart("growth-chart",
-                            {
-                                type: "doughnut",
-                                data:
-                                {
-                                    datasets: [{
-                                        backgroundColor: ["#1de13a", "#a8d6b8"],
-                                        data: [yValues[1], 100 - yValues[1]]
-                                    }]
-                                },
-                                options:
-                                {
-                                    title: {
-                                        display: true,
-                                        text: "Growth"
-                                    }
+                        new Chart("chart", {
+                            type: "bar",
+                            data: {
+                                labels: xValues,
+                                datasets: [{
+                                    backgroundColor: "#9bbee1",
+                                    data: yValues
+                                }]
+                            },
+                            options: {
+                                legend: { display: false },
+                                title: {
+                                    display: true,
+                                    text: "World Wine Production 2018"
                                 }
-                            });
-                        new Chart("revenue-chart",
-                            {
-                                type: "doughnut",
-                                data:
-                                {
-                                    datasets: [{
-                                        backgroundColor: ["#2a8ae9", "#9bbee1"],
-                                        data: [yValues[2], 100 - yValues[2]]
-                                    }]
-                                },
-                                options:
-                                {
-                                    title: {
-                                        display: true,
-                                        text: "Renenue"
-                                    }
-                                }
-                            });
+                            }
+                        });
                     </script>
-
                 </div>
-
-                <div class="total-orders">
-                </div>
-            </div>
-            <h2>Total Revanue</h2>
-            <div class="revenue-diagram">
-                <canvas id="myChart" width="800" height="450" style="width:100%;max-width:600px"></canvas>
-
-                <script>
-                    const xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-
-                    new Chart("myChart", {
-                        type: "line",
-                        data: {
-                            labels: xValues,
-                            datasets: [{
-                                data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-                                borderColor: "red",
-                                fill: false
-                            }, {
-                                data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-                                borderColor: "green",
-                                fill: false
-                            }, {
-                                data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-                                borderColor: "blue",
-                                fill: false
-                            }]
-                        },
-                        options: {
-                            legend: { display: false }
-                        }
-                    });
-                </script>
             </div>
         </div>
     </div>
