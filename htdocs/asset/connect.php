60c7054f160d7c0196239d4cfacd387a2944a4c3
Popier92 <popier92@gmail.com>
2025-01-07 07:38:04 +0800

    connection fix test

--- htdocs/asset/connect.php
+++ htdocs/asset/connect.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Connection failedd: " . $e->getMessage());
 }
 ?>
