a607695bb480c40b119d18670e79e57186e550b9
Popier92 <popier92@gmail.com>
2025-01-07 08:12:07 +0800

    connect fix v2

--- htdocs/asset/connect.php
+++ htdocs/asset/connect.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "if0_38042508";
+$password = "OPhQ0F3THq6o2C";
 $dbname = "db_userdetails";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failedd: " . $e->getMessage());
+    die("Connection failed404: " . $e->getMessage());
 }
 ?>
